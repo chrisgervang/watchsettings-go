a9559d6389307fb80bd58cf374564b49a113e789
Chris Gervang <gervangster@live.com>
2014-10-04 01:15:04 -0700

    truncate

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func loadPage(subdomain string) (*Page, error) {
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home")
-	title := r.Host
+	title := r.Host[:len(".watchsettings.com")]
 	fmt.Println(r.RemoteAddr)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", title, r)
 
