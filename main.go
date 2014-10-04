cf87c322c337bc58dc5223ef90d0097cc02afc36
Chris Gervang <gervangster@live.com>
2014-10-04 01:04:13 -0700

    print

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func loadPage(subdomain string) (*Page, error) {
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home")
 	title := r
+	fmt.Println(r)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", r.URL.Path, title)
 
 }
