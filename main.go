1c66e9b3bf0738f10c80a517869dc8457af9b2d7
Chris Gervang <gervangster@live.com>
2014-10-04 00:54:40 -0700

    fix?

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func loadPage(subdomain string) (*Page, error) {
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home")
-	title := r.URL.Path[:len("watchsettings.com")]
+	title := r.URL.Path
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", r.URL.Path, title)
 
 }
