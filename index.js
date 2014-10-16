5e39d76305f5d63128eabd633536ce913451f6b7
Chris Gervang <gervangster@live.com>
2014-10-16 11:34:14 -0700

    node for firebase & generate html functions

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 //fireware redirection command: iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 
-
-var db = require("./db.js");
-var fs = require("fs");
+//imports
+var db         = require("./db.js");
+var fs         = require("fs");
 var Handlebars = require("handlebars");
 
+//data reference
 var appsRef = new db('apps');
 
-var appRecord = {};
-var startup = false;
+//local 'apps' data record. This reflects the current state of the server.
+var appsRecord = {};
+
+//on ramp up, fill up appsRecord
 appsRef.root.once('value', function (snapshot) {
-    appRecord = snapshot.val();
-    console.log("firebase load all: " + JSON.stringify(appRecord, null, " "));
-    init()
-})
+    appsRecord = snapshot.val();
+    // console.log("firebase load all: " + JSON.stringify(appsRecord, null, " "));
+    console.log("firebase load all");
+    init();
+});
+
 function init() {
+
   appsRef.root.on('child_changed', function (snapshot) {
     console.log(JSON.stringify(snapshot.val(), null, " "));
     var child = snapshot.val();
 
-    if(child.configuration.landing.lastSaved !== appRecord[child.id].configuration.landing.lastSaved) {
+    if(child.configuration.landing.lastSaved !== appsRecord[child.id].configuration.landing.lastSaved) {
       console.log("timestamp changed, regenerate landing html");
-      generate.landingHtml(child.id, child.configuration.landing)
+      generate.landingHtml(child.id, child.configuration.landing, child.settingsList)
     }
 
-  })
+  });
+  
 }
 
 var generate = {
-  landingHtml: function(filename, configuration) {
+  landingHtml: function(filename, configuration, settings) {
     var content;
     // First I want to read the file
     var template = "landing-page";
@@ -40,20 +47,43 @@ var generate = {
 
         // Invoke the next step here however you like
         //console.log(content);   // Put all of the code here (not the best solution)
+        console.log("in landing Html")
         processFile();          // Or put the next step in a function and invoke it
     });
 
     function processFile() {
-        console.log("content: ", content);
-        var result = Handlebars.compile(content.toString());
+        console.log("in process file: ", settings);
 
-        fs.writeFile("./" + filename + '.html', result, function (err) {
+        Handlebars.registerHelper('list', function(items, options) {
+            console.log("in callback!", settings,"items: " , items)
+            var out = '';
+            if(!items){
+                items = settings
+            }
+
+            for(var i=0, l=items.length; i<l; i++) {
+                out = out + options.fn(items[i]);
+            }
+            //callback
+            return out;
+        });
+
+        Handlebars.registerHelper('ifvalue', function (conditional, options) {
+            if (options.hash.value === conditional) {
+                return options.fn(this)
+            } else {
+                return options.inverse(this);
+            }
+        });
+
+        var result = Handlebars.compile(content.toString());
+        //console.log("result: ", result(settings));
+
+        fs.writeFile("./" + filename + '.html', result(), function (err) {
           if (err) return console.log(err);
           console.log('Write complete -> '+filename+'.html');
         });
 
-
-        //console.log("result: ", result);
     }
 
   },
@@ -61,44 +91,3 @@ var generate = {
 
   }
 }
-
-
-var settingsList = [{
-    "type": "title",
-    "data": {
-        "value": "Settings"
-    }
-},
-{
-    "type": "text",
-    "data": {
-        "value": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    }
-},
-{
-    "type": "toggle",
-    "data": {
-        "name": "Show Ads",
-        "id": "showAds"
-    }
-}];
-
-Handlebars.registerHelper('list', function(items, options) {
-    var out = '';
-    if(!items){
-        items = settingsList
-    }
-
-    for(var i=0, l=items.length; i<l; i++) {
-        out = out + options.fn(items[i]);
-    }
-
-    return out;
-});
-Handlebars.registerHelper('ifvalue', function (conditional, options) {
-    if (options.hash.value === conditional) {
-        return options.fn(this)
-    } else {
-        return options.inverse(this);
-    }
-});
